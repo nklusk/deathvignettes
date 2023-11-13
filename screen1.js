18350e0596ef97a3de8ab4b7af18ee9da8ed2c41
NATALIE LUSK <NKL22@fsu.edu>
2023-11-13 11:26:52 -0500

    halfy thru sword snap (end of class)

--- screen1.js
+++ screen1.js
@@ -1,4 +1,6 @@
 let screenOneBackground;
+let sword;
+let handPosition;
 
 function preload () {
     screenOneBackground = loadImage("assets/screenOneBackground.png");
@@ -9,8 +11,10 @@ createCanvas(800, 600);
 
 sword = new Sprite ();
 sword.img = 'assets/screenOneSword.png'
+sword.position = createVector(650, 410);
+sword.friction = 10;
 
-
+handPosition = createVector (500, 315);
 flower = new Sprite ();
 flower.img = 'assets/screenOneFlower.png';
 
@@ -20,17 +24,28 @@ leaves.img = 'assets/screenOneLeaf.png';
 flower.collider = "static";
 leaves.collider = "static";
 
+
 }
 
 function draw() {
     clear();
     background(screenOneBackground);
-    sword.x = 650;
-    sword.y = 410;
+    // sword.x = 650;
+    // sword.y = 410;
     flower.x = 650;
     flower.y = 350;
     leaves.x = 650;
     leaves.y = 458;
-    
-    
+
+    if (sword.mouse.dragging()) {
+        sword.moveTowards(mouseX, mouseY, .1);
+        sword.collider = "kinematic";
+    }
+
+    if (dist(sword.x,sword.y,handPosition.x,handPosition.y) < 20) {
+        console.log("in hand")
+        sword.x = 650;
+        sword.y = 410;
+    }
+
 }
